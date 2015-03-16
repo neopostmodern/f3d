0da804af911bf4baad79287ad1e2fd4a51db274b
neo post modern <clemens@neopostmodern.com>
2015-03-16 17:18:02 +0100

    Masks fixed

--- main.py
+++ main.py
@@ -13,28 +13,33 @@ PATH = os.path.dirname(__file__)
 IMAGE_SIZE = np.array([1920, 1080])
 
 camera = view.Camera(np.array([0, 0, 0]), 55, IMAGE_SIZE)
-surface = view.Surface(np.array([-960, -540, 2000]), IMAGE_SIZE)
 
-container_svg = etree.parse(open("container.svg", mode='r'))
-container_element = (container_svg.find("//*[@id='container']"))
+houseAlpha = view.AlphaMask('textures/houses-alpha.svg', np.array([-960, -540, 2000]), IMAGE_SIZE)
 
-svg_tree = etree.parse(open("scene.svg", 'r'))
-svg_texture = svg_tree.find("//*[@id='texture']")
+houseSurface = view.TexturedSurface('textures/houses.svg', np.array([-960, -540, 2000]), IMAGE_SIZE)
+houseBackgroundSurface = view.TexturedSurface('textures/houses-gray.svg', np.array([-960, -540, 2100]), IMAGE_SIZE, mask_identifier=houseAlpha.identifier)
 
-for z in range(0, 900, 20):
+for z in range(0, 2200, 20):
     camera.position[0] = z / 2
     camera.position[1] = z / -4
     camera.position[2] = z
 
-    intersections = camera.project_surface(surface)
+    container_svg = etree.parse(open("container.svg", mode='r'))
+    svg_container = container_svg.find("//*[@id='container']")
+    defs_container = container_svg.find("//*[@id='defs']")
 
-    element.set("transform", "matrix(%f %f %f %f %f %f)"
-                % tuple(svg_utility.generate_svg_transformation_matrix(intersections)))
-
-
+    defs_container.append(houseAlpha.get_transformed_to(
+        camera.project_surface(houseAlpha)
+    ))
+    svg_container.append(houseBackgroundSurface.get_transformed_to(
+        camera.project_surface(houseBackgroundSurface)
+    ))
+    svg_container.append(houseSurface.get_transformed_to(
+        camera.project_surface(houseSurface)
+    ))
 
     with open("svg/test-%d.svg" % z, mode="w") as output_file:
-        output_file.write(etree.tostring(svg_tree, encoding='unicode'))
+        output_file.write(etree.tostring(container_svg, encoding='unicode'))
 
     # https://inkscape.org/en/doc/inkscape-man.html
     command = ['inkscape',
--- view.py
+++ view.py
@@ -1,7 +1,12 @@
+import copy
 import math
 import uuid
 import numpy as np
+
+from lxml import etree
+
 import rotation_utility
+import svg_utility
 import vector_utility
 
 __author__ = 'neo post modern'
@@ -40,17 +45,67 @@ class Camera:
         return intersection_points
 
 
-
-
 class Surface:
-    def __init__(self, origin, size, rotation=[0, 0, 0]):
+    def __init__(self, origin, size, svg_element, rotation=[0, 0, 0], prefix=""):
         self.origin = origin
         self.size = size
         self.rotation = rotation
 
         self.normalVector = np.array([0, 0, 1])
-
         self.normalVector = rotation_utility.rotate_vector_3d(self.normalVector, rotation)
 
-        self.identifier = uuid.uuid4()
+        self.svgElement = svg_element
+        self.identifier = prefix + str(uuid.uuid4())
+
+    def get_transformed_to(self, mapping):
+        svg_element = copy.deepcopy(self.svgElement)
+        svg_element.set("transform", "matrix(%f %f %f %f %f %f)"
+                            % tuple(svg_utility.generate_svg_transformation_matrix(mapping)))
+        svg_element.set("id", self.identifier)
+        return svg_element
+
+
+class TexturedSurface(Surface):
+    def __init__(self, texture_path, origin, size, rotation=[0, 0, 0], mask_identifier=None):
+        with open(texture_path, 'r') as svg_file:
+            svg_tree = etree.parse(svg_file)
+            texture = svg_tree.find("//*[@id='texture']")
+
+            if texture is None:
+                raise ValueError("SVG file without texture!")
+
+            Surface.__init__(self, origin, size, texture, rotation, prefix="textured-")
+
+            self.maskIdentifier = mask_identifier
+
+    def get_transformed_to(self, mapping):
+        svg_texture = super().get_transformed_to(mapping)
+
+        if self.maskIdentifier is not None:
+            mask_wrapper = etree.Element("g")
+            mask_wrapper.set("mask", "url(#%s)" % self.maskIdentifier)
+            mask_wrapper.append(svg_texture)
+            return mask_wrapper
+        else:
+            return svg_texture
+
+
+class AlphaMask(Surface):
+    def __init__(self, mask_path, origin, size, rotation=[0, 0, 0]):
+        with open(mask_path, 'r') as svg_file:
+            svg_tree = etree.parse(svg_file)
+            texture = svg_tree.find("//*[@id='mask']")
+
+            if texture is None:
+                raise ValueError("SVG file without mask!")
+
+            Surface.__init__(self, origin, size, texture, rotation, prefix="mask-")
+
+    def get_transformed_to(self, mapping):
+        svg_mask = super().get_transformed_to(mapping)
+
+        mask_wrapper = etree.Element("mask")
+        mask_wrapper.set("id", self.identifier)
+        mask_wrapper.append(svg_mask)
+        return mask_wrapper
 
