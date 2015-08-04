55bcd5493a2981adbcb971785511dc41b3969cbf
neo post modern <clemens@neopostmodern.com>
2015-08-04 21:48:06 +0100

    Transparent merges parallelized

--- f3d/rendering/png_service.py
+++ f3d/rendering/png_service.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
+from multiprocessing.pool import Pool
 import os
 import subprocess
 import tempfile
@@ -14,6 +15,30 @@ SLIMER_EXECUTABLE = os.path.join(os.path.dirname(os.path.realpath(__file__)), ".
 # todo: enable transparent rendering
 # one (bad) approach: https://github.com/laurentj/slimerjs/issues/154#issuecomment-58495876
 COLORS = ["#0000FF", "#FFFF00"]
+
+
+# hack: must be a top level function for pickling to work
+# http://stackoverflow.com/a/1816969/2525299
+def merge_frame(frame_index):
+    file_name = FileManagement.png_file_path_for_frame(frame_index)
+
+    input_file_names = [
+        PngService.file_name_for_colored_background(file_name, COLORS[0]),
+        PngService.file_name_for_colored_background(file_name, COLORS[1])
+    ]
+
+    merged_file_name = FileManagement.png_file_path_for_frame(frame_index)
+
+    command = "convert %s %s -alpha off " \
+        "\( -clone 0,1 -compose difference -composite -separate -evaluate-sequence max -auto-level -negate \) " \
+        "\( -clone 0,2 -fx \"v==0?0:u/v-u.p{0,0}/v+u.p{0,0}\" \) " \
+        "-delete 0,1 +swap -compose Copy_Opacity -composite %s" % (
+            input_file_names[0],
+            input_file_names[1],
+            merged_file_name
+        )
+
+    return os.system(command), merged_file_name
 
 
 class PngService:
@@ -42,8 +67,9 @@ class PngService:
     def __slimer_initialization_code():
         return "page.viewportSize = { width: 1920, height: 1080 };"
 
+    # hack: make private again, but currently global `merge_frames` function depends on this
     @staticmethod
-    def __file_name_for_colored_background(file_name, color):
+    def file_name_for_colored_background(file_name, color):
         return file_name.replace(".png", "-%s.png" % color.replace('#', ''))
 
 
@@ -56,24 +82,19 @@ class PngService:
         document.body.style.background = '%s';
     });
     page.render('%s', { onlyViewport: true });
-            """ % (color, PngService.__file_name_for_colored_background(file_name, color))
+            """ % (color, PngService.file_name_for_colored_background(file_name, color))
         return code
 
     @staticmethod
     def __merge_into_transparent_frames(frame_indices):
-        for frame_index in frame_indices:
-            file_name = FileManagement.png_file_path_for_frame(frame_index)
-            command = """
-    convert %s %s -alpha off \( -clone 0,1 -compose difference -composite -separate -evaluate-sequence max -auto-level -negate \) \( -clone 0,2 -fx "v==0?0:u/v-u.p{0,0}/v+u.p{0,0}" \) -delete 0,1 +swap -compose Copy_Opacity -composite %s
-            """ % (
-                PngService.__file_name_for_colored_background(file_name, COLORS[0]),
-                PngService.__file_name_for_colored_background(file_name, COLORS[1]),
-                FileManagement.png_file_path_for_frame(frame_index)
-            )
+        # todo: switch to pathos when 3.x compliant - https://github.com/uqfoundation/pathos/issues/1
+        with Pool(processes=4) as pool:
+            for return_value, output_file in pool.imap_unordered(merge_frame, frame_indices):
+                if return_value != 0:
+                    logging.debug("Frame merge to transparent exited with %d for '%s'" % (return_value, output_file))
 
-            logging.debug(command)
-
-            os.system(command)
+        # for frame_index in frame_indices:
+        #     merge_frame(frame_index)
 
     def render_svg_to_png(self, frame_index):
         slimer_commands = """
